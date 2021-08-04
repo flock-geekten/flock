ffa2e95df3719e67348cf11d8a9abed80ec5a37e
oba18 <m.oba.0426@gmail.com>
2021-08-04 13:50:58 +0900

    wip

---
+++ view/flock_view/components/Menu.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <v-card
+      class="mx-auto"
+      max-width="300"
+      tile
+      flat
+      >
+      <v-list shaped color="blue-grey lighten-5">
+        <v-subheader>メニュー</v-subheader>
+        <v-list-item-group
+          v-model="selectedItem"
+          color="primary"
+          >
+          <v-list-item
+            v-for="(item, i) in items"
+            :key="i"
+            >
+            <v-list-item-icon>
+              <v-icon v-text="item.icon"></v-icon>
+            </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.text"></v-list-item-title>
+          </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        { text: "ホーム" },
+        { text: "あそび一覧" }
+      ]
+    }
+  }
+}
+</script>
--- view/flock_view/pages/index.vue
+++ view/flock_view/pages/index.vue
@@ -1,77 +1,71 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <v-card class="logo py-4 d-flex justify-center">
-        <NuxtLogo />
-        <VuetifyLogo />
-      </v-card>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <div>
+    <div class=bg>aaaaaa</div>
+    <v-row>
+      <v-col cols="3">
+        <Menu />
+      </v-col>
+      <v-col cols="9">
+        <v-row align="center" class="justify-center">
+          <v-col cols="12" sm="8" md="6">
+            <v-card class="logo py-4 d-flex justify-center" color="white">
+              <v-card-title>あそびのタイトル</v-card-title>
+            </v-card>
+            <br>
+            <v-card class="logo py-4 d-flex justify-center" color="white">
+              <v-card-title>あそびのタイトル</v-card-title>
+            </v-card>
+            <br>
+            <v-card class="logo py-4 d-flex justify-center" color="white">
+              <v-card-title>あそびのタイトル</v-card-title>
+            </v-card>
+            <br>
+            <v-card class="logo py-4 d-flex justify-center" color="white">
+              <v-card-title>あそびのタイトル</v-card-title>
+            </v-card>
+            <br>
+            <v-card class="logo py-4 d-flex justify-center" color="white">
+              <v-card-title>あそびのタイトル</v-card-title>
+            </v-card>
+            <br>
+            <v-card class="logo py-4 d-flex justify-center" color="white">
+              <v-card-title>あそびのタイトル</v-card-title>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </div>
 </template>
+
+<script>
+import Menu from '../components/Menu.vue'
+export default {
+  components: {
+    Menu
+  }
+}
+</script>
+
+<style>
+bg {
+  /* 画像ファイルの指定 */
+  /* background-image: url(../assets/sky.jpg); */
+
+  /* 画像を常に天地左右の中央に配置 */
+  background-position: center center;
+
+  /* 画像をタイル状に繰り返し表示しない */
+  background-repeat: no-repeat;
+
+  /* コンテンツの高さが画像の高さより大きい時、動かないように固定 */
+  background-attachment: fixed;
+
+  /* 表示するコンテナの大きさに基づいて、背景画像を調整 */
+  background-size: cover;
+
+  /* 背景画像が読み込まれる前に表示される背景のカラー */
+  background-color: #464646;
+
+}
+</style>

beb5b094a13003a35fa6e83fe52f1f0afb83e5f2
oba18 <m.oba.0426@gmail.com>
2021-09-03 12:22:17 +0900

    [add] docker-compose.prod.yml

--- view/flock_view/plugins/constants.js
+++ view/flock_view/plugins/constants.js
@@ -1,5 +1,5 @@
-const apiBaseUrl = 'http://localhost:3000'
-const summaryBaseUrl = 'http://localhost:9000'
+const apiBaseUrl = 'https://flock-api.masashi-oba.net'
+const summaryBaseUrl = 'https://flock.masashi-oba.net:9000'
 
 export default (context, inject) => {
   // 定数を共通化  

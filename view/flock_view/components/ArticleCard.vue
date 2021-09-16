9899c8aab48023956b4b9a566e80d97a991e62a7
oba18 <m.oba.0426@gmail.com>
2021-09-16 19:41:22 +0900

    [mod] 投稿のカードをコンポーネント化

---
+++ view/flock_view/components/ArticleCard.vue
@@ -0,0 +1,76 @@
+<template >
+    <div>
+        <v-card 
+          color="white" 
+          flat
+          rounded
+          height="320"
+          max-height="320"
+          :to="{
+               name: 'articles-id',
+               params: {
+               id: post.post.id
+               }
+               }"
+          class="pa-8" 
+          :ripple="false"
+          >
+          <v-row>
+            <v-card-title><h2>{{ post.post.title | omittedText35 }}</h2></v-card-title>
+          </v-row>
+          <v-row>
+            <v-col cols="3">
+              <v-row>
+                <v-img
+                  class="my-3"
+                  max-height="200"
+                  max-width="200"
+                  src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+                  ></v-img>
+              </v-row>
+              <v-row>
+<v-card-text><v-icon class="mr-1" color="pink">mdi-heart-outline</v-icon>{{ post.likes_count }}<v-icon class="ml-3 mr-1" color="orange">mdi-comment-outline</v-icon>{{ post.comments_count }}</v-card-text>
+              </v-row>
+            </v-col>
+            <v-col cols="9">
+              <v-row>
+                <v-chip-group class="ml-3"><v-chip>{{ post.tags[0].name | omittedText5 }}</v-chip><v-chip>{{ post.tags[1].name | omittedText5 }}</v-chip><v-chip>{{ post.tags[2].name | omittedText5 }}</v-chip><v-chip>{{ post.tags[3].name | omittedText5 }}</v-chip><v-chip>{{ post.tags[4].name | omittedText5 }}</v-chip></v-chip-group>
+              </v-row>
+              <v-row>
+                <v-card-text><p style="font-size:20px; line-height:28px">{{ post.summary.content | omittedText200 }}</p></v-card-text>
+              </v-row>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col cols="12">
+            </v-col>
+          </v-row>
+        </v-card>
+    </div>
+</template>
+
+<script>
+export default{
+  props: {
+    post: Object
+  },
+  filters: {
+    omittedText200(text) {
+     // 200文字目以降は"…"
+     return text.length > 200 ? text.slice(0, 200) + "…" : text;
+    },
+    omittedText5(text) {
+     // 20文字目以降は"…"
+     return text.length > 5 ? text.slice(0, 5) + "…" : text;
+    },
+    omittedText80(text) {
+     // 80文字目以降は"…"
+     return text.length > 80 ? text.slice(0, 80) + "…" : text;
+    },
+    omittedText35(text) {
+     // 80文字目以降は"…"
+     return text.length > 35 ? text.slice(0, 35) + "…" : text;
+    },
+  },
+}
+</script>
--- view/flock_view/pages/tags/_id.vue
+++ view/flock_view/pages/tags/_id.vue
@@ -3,39 +3,7 @@
     <h1 class="py-5">{{ tag.name }}に関する記事</h1>
     <v-row align="center" class="justify-center">
       <v-col cols="12" v-for="post in posts" :key="post.id">
-        <v-card 
-          color="white" 
-          flat
-          height="350"
-          max-height="800"
-          :ripple="false"
-          :to="{
-               name: 'articles-id',
-               params: {
-               id: post.post.id
-               }
-               }"
-          class="pa-5"
-          >
-          <v-row>
-            <v-col cols="3">
-              <v-img
-                max-height="300"
-                max-width="300"
-                src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-                ></v-img>
-            </v-col>
-            <v-col cols="9">
-              <v-card-title>{{ post.post.title | omittedText20 }}</v-card-title>
-              <v-card-text>{{ post.summary.content | omittedText200 }}</v-card-text>
-            </v-col>
-          </v-row>
-          <v-row>
-            <v-col cols="12">
-              <v-chip>{{ post.tags[0].name | omittedText20 }}</v-chip> <v-chip>{{ post.tags[1].name | omittedText20 }}</v-chip> <v-chip>{{ post.tags[2].name | omittedText20 }}</v-chip><v-spacer /><v-card-text><v-icon class="mr-1" color="pink">mdi-heart-outline</v-icon>{{ post.likes_count }}<v-icon class="ml-3 mr-1" color="orange">mdi-comment-outline</v-icon>{{ post.comments_count }}</v-card-text>
-            </v-col>
-          </v-row>
-        </v-card>
+        <ArticleCard :post="post" />
       </v-col>
     </v-row>
   </div>
@@ -43,7 +11,11 @@
 
 <script>
 import axios from 'axios'
+import ArticleCard from '../../components/ArticleCard.vue'
 export default {
+  components: {
+    ArticleCard
+  },
   data () {
     return {
       tag: '',

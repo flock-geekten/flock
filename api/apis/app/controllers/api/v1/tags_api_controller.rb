b2c94eac6484f032d077b860bb83b784ced5517b
oba18 <m.oba.0426@gmail.com>
2021-09-18 05:36:25 +0900

    [mod]

--- api/apis/app/controllers/api/v1/tags_api_controller.rb
+++ api/apis/app/controllers/api/v1/tags_api_controller.rb
@@ -2,14 +2,15 @@ class Api::V1::TagsApiController < ApplicationController
 
   def index
     @tag = Tag.find(params[:tag_id])
-    @posts = @tag.posts.order("RAND()").limit(5)
+    @posts = @tag.posts
     post_summary_dict = []
     for post in @posts
       summary = post.summary
       comments_count = post.comments.count
       likes_count = post.likes.count
       tags = post.tags
-      post_summary_dict << { post: post, summary: summary, comments_count: comments_count, likes_count: likes_count, tags: tags }
+			flock = post.flock
+      post_summary_dict << { post: post, summary: summary, comments_count: comments_count, likes_count: likes_count, tags: tags, flock: flock }
     end
     render json: { tag: @tag, posts: post_summary_dict }
   end
